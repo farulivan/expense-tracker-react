99ca3567f8a362ab95e944db3f4fcb8930c1d30a
Farul Ivan <farulivan@gmail.com>
2023-01-14 14:50:54 +0700

    adding key

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,6 +17,7 @@ const Expenses = (props) => {
       <ExpensesFilter selectedYear={filteredYear} onFilter={filterHandler} />
       {props.item.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
